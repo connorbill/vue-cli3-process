e729fd9f57eb531af26430931f1c88ed6ece44e6
example <example@gmail.com>
2019-09-05 22:40:45 +0800

    jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,3 @@
-
 module.exports = {
   root: true,
   env: {
@@ -11,14 +10,44 @@ module.exports = {
     "prettier"
   ],
   extends: [
-    "prettier",
     "plugin:vue/recommended",
-    // "plugin:prettier/recommended",
+    "standard",
+    "prettier",
+    "prettier/standard",
+    "prettier/vue",
   ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "prettier/prettier": ["error"],
+    'vue/require-default-prop': 'off',
+    // 'vue/array-bracket-spacing': 'error',
+    // 'vue/arrow-spacing': 'error',
+    // 'vue/block-spacing': 'error',
+    // 'vue/brace-style': 'error',
+    // 'vue/camelcase': 'error',
+    // 'vue/comma-dangle': ['error', 'always-multiline'],
+    // 'vue/component-name-in-template-casing': 'error',
+    // 'vue/dot-location': ['error', 'property'],
+    // 'vue/eqeqeq': 'error',
+    // 'vue/key-spacing': 'error',
+    // 'vue/keyword-spacing': 'error',
+    // 'vue/no-boolean-default': ['error', 'default-false'],
+    // 'vue/no-deprecated-scope-attribute': 'error',
+    // 'vue/no-empty-pattern': 'error',
+    // 'vue/object-curly-spacing': ['error', 'always'],
+    // 'vue/space-infix-ops': 'error',
+    // 'vue/space-unary-ops': 'error',
+    // 'vue/v-on-function-call': 'error',
+    // 'vue/v-slot-style': [
+    //   'error',
+    //   {
+    //     atComponent: 'v-slot',
+    //     default: 'v-slot',
+    //     named: 'longform',
+    //   },
+    // ],
+    // 'vue/valid-v-slot': 'error',
   },
   parserOptions: {
     "parser": "babel-eslint",
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 // vue.config.js
-const path = require('path')
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const CompressionWebpackPlugin = require("compression-webpack-plugin");
-
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
 
 // 假设 http://localhost:5000 为 cdn 域名
-const publicPath = process.env.NODE_ENV === "production" ? "http://localhost:5000" : "/";
+const publicPath =
+  process.env.NODE_ENV === 'production' ? 'http://localhost:5000' : '/';
 // 是否使用gzip
 const productionGzip = true;
 // 需要gzip压缩的文件后缀
 const productionGzipExtensions = ['js', 'css'];
-
 
 // <script src="//shadow.elemecdn.com/npm/vue@2.5.16/dist/vue.runtime<%= process.env.NODE_ENV === 'production' ? '.min.js' : '.js' %>" crossorigin="anonymous"></script>
 //   <script src="//shadow.elemecdn.com/npm/vue-router@3.0.1/dist/vue-router<%= process.env.NODE_ENV === 'production' ? '.min.js' : '.js' %>" crossorigin="anonymous"></script>
@@ -22,48 +22,47 @@ const cdn = {
   dev: {
     css: [],
     js: [
-      "//shadow.elemecdn.com/npm/vue@2.5.16/dist/vue.runtime.js",
-      "//shadow.elemecdn.com/npm/vue-router@3.0.1/dist/vue-router.js",
-      "//unpkg.com/vuex@3.1.1/dist/vuex.js",
+      '//shadow.elemecdn.com/npm/vue@2.5.16/dist/vue.runtime.js',
+      '//shadow.elemecdn.com/npm/vue-router@3.0.1/dist/vue-router.js',
+      '//unpkg.com/vuex@3.1.1/dist/vuex.js'
     ]
   },
   // 生产环境
   build: {
     css: [],
     js: [
-      "//shadow.elemecdn.com/npm/vue@2.5.16/dist/vue.runtime.min.js",
-      "//shadow.elemecdn.com/npm/vue-router@3.0.1/dist/vue-router.min.js",
-      "//unpkg.com/vuex@3.1.1/dist/vuex.min.js",
+      '//shadow.elemecdn.com/npm/vue@2.5.16/dist/vue.runtime.min.js',
+      '//shadow.elemecdn.com/npm/vue-router@3.0.1/dist/vue-router.min.js',
+      '//unpkg.com/vuex@3.1.1/dist/vuex.min.js'
     ]
   }
 };
-
 
 module.exports = {
   publicPath: publicPath,
   productionSourceMap: false,
   chainWebpack: config => {
     // 所有组件中导入一些文件，不用每个组件都引入一个东西，估计用的少
-    const types = ["vue-modules", "vue", "normal-modules", "normal"];
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
     types.forEach(type =>
-      addStyleResource(config.module.rule("scss").oneOf(type))
+      addStyleResource(config.module.rule('scss').oneOf(type))
     );
     // svg-loader
-    const svgRule = config.module.rule("svg");
+    const svgRule = config.module.rule('svg');
     // 清除已有的所有 loader。
     // 如果你不这样做，接下来的 loader 会附加在该规则现有的 loader 之后。
     svgRule.uses.clear();
     // 添加要替换的 loader
-    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+    svgRule.use('vue-svg-loader').loader('vue-svg-loader');
 
     /**
      * 添加CDN参数到htmlWebpackPlugin配置中
      */
-    config.plugin("html").tap(args => {
-      if (process.env.NODE_ENV === "production") {
+    config.plugin('html').tap(args => {
+      if (process.env.NODE_ENV === 'production') {
         args[0].cdn = cdn.build;
       }
-      if (process.env.NODE_ENV === "development") {
+      if (process.env.NODE_ENV === 'development') {
         args[0].cdn = cdn.dev;
       }
       return args;
@@ -83,28 +82,26 @@ module.exports = {
       }
     }
   },
-  configureWebpack:  () => {
+  configureWebpack: () => {
     var myConfig = {
       plugins: [],
       externals: {}
     };
     // 执行 --mode analysis 命令时，显示打包图
-    if (process.env.VUE_APP_TYPE === "analysis"){
+    if (process.env.VUE_APP_TYPE === 'analysis') {
       myConfig.plugins.push(new BundleAnalyzerPlugin());
     }
     // 将公用库抽离出去，并将链接放入index.html
     myConfig.externals = {
-      vue: "window.Vue",
-      "vue-router": "window.VueRouter",
-      "vuex": "window.Vuex"
+      vue: 'window.Vue',
+      'vue-router': 'window.VueRouter',
+      vuex: 'window.Vuex'
       // 其他三方库 ...
     };
     productionGzip &&
       myConfig.plugins.push(
         new CompressionWebpackPlugin({
-          test: new RegExp(
-            "\\.(" + productionGzipExtensions.join("|") + ")$"
-          ),
+          test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
           threshold: 8192,
           minRatio: 0.8
         })
@@ -115,21 +112,9 @@ module.exports = {
 
 function addStyleResource(rule) {
   rule
-    .use("style-resource")
-    .loader("style-resources-loader")
+    .use('style-resource')
+    .loader('style-resources-loader')
     .options({
-      patterns: [path.resolve(__dirname, "./src/assets/styles/imports.scss")]
+      patterns: [path.resolve(__dirname, './src/assets/styles/imports.scss')]
     });
 }
-
-// "@vue/cli-plugin-eslint": "^3.11.0",
-//"@vue/eslint-config-prettier": "^5.0.0",
-//"babel-eslint": "^10.0.1",
-// "eslint": "^5.16.0",
-//   "eslint-config-prettier": "^6.1.0",
-//   "eslint-plugin-json": "^1.4.0",
-//   "eslint-plugin-prettier": "^3.1.0",
-//   "eslint-plugin-vue": "^5.0.0",
-//"lint-staged": "^8.1.5",
-// "prerender-spa-plugin": "^3.4.0",
-//   "prettier": "^1.18.2",
